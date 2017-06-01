7a4e9158fc3d8442c73241b6a1369d0f6ce7a402
Manel Romero <manel@manelromero.com>
2017-06-02 01:18:29 +0200

    Add MongoDB

--- system/teams/repository.rb
+++ system/teams/repository.rb
@@ -1,12 +1,24 @@
+require 'mongo'
+
 module Teams
   class Repository
-    @content = []
+    URI = ENV['MONGODB_URI']
 
     class << self
       def add(team)
-        @content << team
+        collection.insert_one(team.serialize)
         team
       end
+
+    private
+
+    def connection
+      @connection ||= Mongo::Client.new(URI)
+    end
+
+    def collection
+      connection[:teams]
+    end
     end
   end
 end
